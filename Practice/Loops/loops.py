d57cd8a7e7e76e4692b2b4f151117d1b63264055
Amit Biswas <amit.142biswas@gmail.com>
2024-02-14 12:24:00 +0530

    loop completed

--- Practice/Loops/loops.py
+++ Practice/Loops/loops.py
@@ -42,10 +42,53 @@
 #         break
 
 #6th factorial
-number = 5
-fact = 1
-while number > 0:
-    fact = fact * number
-    number = number - 1
+# number = 5
+# fact = 1
+# while number > 0:
+#     fact = fact * number
+#     number = number - 1
     
-print("Factorial", fact)
+# print("Factorial", fact)
+
+#7th
+# while True:
+#     number = int(input("Enter b/w 1 n 10: "))
+#     if 1 <= number <= 10:
+#         print("Thanks")
+#         break
+#     else:
+#         print("Try again")
+
+#8th Prime no
+# n = 29
+# prime = True
+# if n > 1:
+#     for i in range(2, n):
+#         if(n % i) == 0:
+#             prime = False
+#             break
+        
+# print(prime)
+
+#9th dupicate item in array
+# items = ["a", "b", "a"]
+
+# unique = set()
+
+# for item in items:
+#     if item in unique:
+#         print("Duplicate: ", item)
+#         break
+#     unique.add(item)
+
+#10th exponential backoff (wait time increment)
+# import time
+# wait = 1
+# max_try = 5
+# att = 0
+
+# while att < max_try:
+#     print("Attempt", att + 1, "wait time", wait)
+#     time.sleep(wait)
+#     wait *= 2
+#     att += 1
